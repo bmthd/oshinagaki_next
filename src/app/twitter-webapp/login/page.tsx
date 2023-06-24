7d876998615d41e976685f2e718e464870af2d7f
bmthd <jougennotuki67@gmail.com>
2023-06-25 01:02:25 +0900

    login auth

--- src/app/twitter-webapp/login/page.tsx
+++ src/app/twitter-webapp/login/page.tsx
@@ -1,14 +1,38 @@
-"use client"
+"use client";
 
-import { NextPage } from 'next'
-import { useEffect } from 'react';
+import { NextPage } from "next";
+import { useEffect } from "react";
 
-const RedirectPage:NextPage = () => {
+const RedirectPage: NextPage = () => {
+  useEffect(() => {
+    window.location.href = getRedirectUrl();
+  }, []);
+  return <div>Redirecting...</div>;
+};
 
-    useEffect(() => {
-        window.location.href = "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=R1lkampvZkFtalQ4SUg3TWlNeEI6MTpjaQ&redirect_uri=https://oshinagaki.bmth.dev/twitter-webapp/callback&scope=offline.access%20users.read%20follows.read&state=state&code_challenge=challenge&code_challenge_method=plain"
-    }, [])
-    return <div>Redirecting...</div>
-}
+const getRedirectUrl = () => {
+  const auth_url = "https://twitter.com/i/oauth2/authorize";
+  const params = {
+    response_type: "code",
+    client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
+    redirect_uri:
+      "https://vercel.com/bmthd/oshinagaki-front/api/auth/callback/twitter",
+    scope: "offline.access%20users.read%20follows.read",
+    state: "state",
+    code_challenge: "challenge",
+    code_challenge_method: "plain",
+  };
+
+  const queryString = Object.entries(params)
+    .map(
+      ([key, value]) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(value || "")}`
+    )
+    .join("&");
+
+  const redirectUrl = `${auth_url}?${queryString}`;
+
+  return redirectUrl;
+};
 
 export default RedirectPage;
