f9fa2e05749fe19798fda6f5b4a5d1b113674c03
bmthd <jougennotuki67@gmail.com>
2023-07-22 20:38:51 +0900

    設定更新

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        outputFileTracingExcludes: {
+         "*": [
+          "node_modules/@swc/core-linux-x64-gnu",
+          "node_modules/@swc/core-linux-x64-musl",
+          "node_modules/@esbuild/linux-x64",
+         ],
+        },
+       },
 }
 
 module.exports = nextConfig
