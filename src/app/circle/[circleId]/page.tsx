73356d082bb5b42d22e3af5eb7859b095641bc52
bmthd <jougennotuki67@gmail.com>
2023-08-08 00:24:05 +0900

    circlePage generateStaticParams create

--- src/app/circle/[circleId]/page.tsx
+++ src/app/circle/[circleId]/page.tsx
@@ -1,11 +1,13 @@
 import { DotHeading, Section, TextLink, TitleHeading } from "@/components/common";
 import { convertToNumber } from "@/lib/util";
 import { fetchCircle, fetchSpacesByCircle } from "@/services/eventService";
+import { fetchCircleIds } from "@/services/slugService";
 import { Suspense } from "react";
 
 export const generateStaticParams = async () => {
-    const circleCount = await fetchCircleCount();
-
+  const circleIds = await fetchCircleIds();
+  return circleIds.map((circleId) => ({ circleId: circleId }));
+};
 
 const Page = async ({ params }: { params: { circleId: string } }) => {
   const circleId = convertToNumber(params.circleId);
