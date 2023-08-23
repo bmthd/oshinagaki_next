a8e4cbe498016e88ae7c4a4fb616862d1332eb04
bmthd <jougennotuki67@gmail.com>
2023-08-23 23:30:17 +0900

    fix cache

--- src/lib/nextCache.ts
+++ src/lib/nextCache.ts
@@ -8,7 +8,6 @@ export function cache<T extends (...args: any[]) => any>(
   }
 ): T {
   return ((...args: Parameters<T>) => {
-    const cacheKey = JSON.stringify(fn) + JSON.stringify(args);
-    return unstable_cache(fn, [cacheKey], options)(...args);
+    return unstable_cache(fn, undefined, options)(...args);
   }) as T;
 }
