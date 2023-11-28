46181011d1fad5365b978f0be89786acb6655b14
bmthd <jougennotuki67@gmail.com>
2023-11-29 02:47:19 +0900

    fix:タイトルの幅

--- src/components/TitleHeading.tsx
+++ src/components/TitleHeading.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 export const TitleHeading = ({ children, className, id }: Props) => {
-  const defaultClassName = `text-2xl font-bold bg-secondary p-2 text-white text-center rounded-md ${className}`;
+  const defaultClassName = `text-2xl w-full font-bold bg-secondary p-2 text-white text-center rounded-md ${className}`;
   return (
     <H className={defaultClassName} id={id}>
       {children}
