93a5f5b675ca01f691e166c7c7120bc613724ddf
bmthd <jougennotuki67@gmail.com>
2025-08-03 12:36:02 +0900

    chore: update Next.js configuration and dependencies
    - Removed experimental settings from next.config.js.
    - Updated @prisma/client and prisma dependencies to version 6.13.0.
    - Upgraded Next.js to version 15.4.4.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        underscore: "lodash",
-        mocha: { browser: "mocha/browser-entry.js" },
-      },
-    },
-    outputFileTracingExcludes: {
-      "*": [
-        "node_modules/@swc/core-linux-x64-gnu",
-        "node_modules/@swc/core-linux-x64-musl",
-        "node_modules/@esbuild/linux-x64",
-      ],
-    },
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "pbs.twimg.com" },
